a608f19406e678608011a228fb299969b013802c
lucasvieirap <ericvpalavro@gmail.com>
2023-05-08 16:15:35 -0300

    Commit inicial

---
+++ src/pages/PaginaBase/index.js
@@ -0,0 +1,22 @@
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
+import FavoritosProvider from "contextos/Favoritos";
+import { Outlet } from "react-router-dom";
+import styles from "./PaginaBase.module.css";
+
+function PaginaBase() {
+    return (
+        <main className={styles.container}>
+            <Cabecalho />
+            <FavoritosProvider>
+                <Container>
+                    <Outlet />
+                </Container>
+            </FavoritosProvider>
+            <Rodape />
+        </main>
+    )
+}
+
+export default PaginaBase;
